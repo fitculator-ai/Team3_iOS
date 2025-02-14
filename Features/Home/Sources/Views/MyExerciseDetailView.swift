192fe3dc7d8f8d27b7423f983b1185a05f709e23
the-hye <ganzi410@gmail.com>
2025-02-14 11:24:33 +0900

    Feat: 나의운동기록 상세 NavigationLink 생성

---
+++ Features/Home/Sources/Views/MyExerciseDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  MyExerciseDetailView.swift
+//  Features
+//
+//  Created by JIHYE SEOK on 2/14/25.
+//
+
+import SwiftUI
+
+struct MyExerciseDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyExerciseDetailView()
+}
