ee421de3a36b4fe529776918c07e3ec7350a6716
song <nadana0929@gmail.com>
2025-02-14 11:32:11 +0900

    Feat: 투명도 추가, 셀 색상 추가

--- Core/Utils/Extensions/Color+Extension.swift
+++ Core/Utils/Extensions/Color+Extension.swift
@@ -10,7 +10,7 @@ import SwiftUI
 // MARK: hex color 값 사용
 // 사용예시 - Color("hex: #FFFFFF")
 public extension Color {  // <- public 추가
-    init(hex: String) {
+    init(hex: String, opacity: Double = 1.0) {
         let scanner = Scanner(string: hex)
         _ = scanner.scanString("#")
         
@@ -20,6 +20,6 @@ public extension Color {  // <- public 추가
         let r = Double((rgb >> 16) & 0xFF) / 255.0
         let g = Double((rgb >>  8) & 0xFF) / 255.0
         let b = Double((rgb >>  0) & 0xFF) / 255.0
-        self.init(red: r, green: g, blue: b)
+        self.init(red: r, green: g, blue: b, opacity: opacity)
     }
 }
