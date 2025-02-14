7d666651925a1412ce0296f9c2749af9e907422c
Jeongheeji <w030w58@ gmail.com>
2025-02-14 11:41:45 +0900

    Feat: 프로필 편집 View(ProfileEditView) 추가

---
+++ Features/Profile/Sources/ProfileEditView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileEditView.swift
+//  Features
+//
+//  Created by Heeji Jung on 2/14/25.
+//
+
+import SwiftUI
+
+struct ProfileEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileEditView()
+}
