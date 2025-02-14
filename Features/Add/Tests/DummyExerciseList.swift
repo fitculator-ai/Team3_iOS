7317d4a4eb7c5f91b579d7b77b74f735b27c88fa
song <nadana0929@gmail.com>
2025-02-14 11:23:53 +0900

    Feat: 리스트 파일 정리

---
+++ Features/Add/Tests/DummyExerciseList.swift
@@ -0,0 +1,22 @@
+//
+//  DummyExerciseList.swift
+//  Fitculator
+//
+//  Created by Song Kim on 2/14/25.
+//
+
+import SwiftUI
+import Core
+
+var aerobicList: [Exercise] = [
+    Exercise(exerciseName: "Running", exerciseType: "Cardio", exerciseColor: "#FF0000", exerciseImage: "figure.run"),
+    Exercise(exerciseName: "Cycling", exerciseType: "Cardio", exerciseColor: "#3498DB", exerciseImage: "bicycle"),
+    Exercise(exerciseName: "Swimming", exerciseType: "Cardio", exerciseColor: "#1ABC9C", exerciseImage: "figure.pool.swim"),
+    Exercise(exerciseName: "Jump Rope", exerciseType: "Cardio", exerciseColor: "#F1C40F", exerciseImage: "figure.jumprope"),
+    Exercise(exerciseName: "Rowing", exerciseType: "Cardio", exerciseColor: "#27AE60", exerciseImage: "figure.rower"),
+    Exercise(exerciseName: "Hiking", exerciseType: "Cardio", exerciseColor: "#8B4513", exerciseImage: "figure.hiking"),
+    Exercise(exerciseName: "Elliptical", exerciseType: "Cardio", exerciseColor: "#9B59B6", exerciseImage: "figure.elliptical"),
+    Exercise(exerciseName: "Stair Climbing", exerciseType: "Cardio", exerciseColor: "#E67E22", exerciseImage: "figure.stair.stepper"),
+    Exercise(exerciseName: "Dancing", exerciseType: "Cardio", exerciseColor: "#E91E63", exerciseImage: "figure.dance"),
+    Exercise(exerciseName: "Kickboxing", exerciseType: "Cardio", exerciseColor: "#7F8C8D", exerciseImage: "figure.kickboxing")
+]
