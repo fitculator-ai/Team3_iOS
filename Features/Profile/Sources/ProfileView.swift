69df3c9bea28f7fc42b661ee1f0a4abbbe830eb2
Jeongheeji <w030w58@ gmail.com>
2025-02-14 10:29:56 +0900

    Feat: 프로필 화면을 위한 Swift 파일 생성

---
+++ Features/Profile/Sources/ProfileView.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileView.swift
+//  Features
+//
+//  Created by Heeji Jung on 2/14/25.
+//
+
+import SwiftUI
+
+public struct ProfileView: View {
+    public init() {}
+    public var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
