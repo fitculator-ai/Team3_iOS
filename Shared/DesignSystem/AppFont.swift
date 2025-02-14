ab2ad93e38d6eaea1292dede55672983d954ae8e
Jeongheeji <w030w58@ gmail.com>
2025-02-14 11:42:26 +0900

    Feat: 프로필(마이) 페이지 사용 폰트 스타일 추가

--- Shared/DesignSystem/AppFont.swift
+++ Shared/DesignSystem/AppFont.swift
@@ -10,4 +10,8 @@ import SwiftUI
 public struct AppFont {
     public static let mainTitle = Font.title2.weight(.black)
     public static let subTitle = Font.title3.weight(.bold)
+    
+    //Profile
+    public static let profileItemFont = Font.title2.weight(.bold)
+    public static let profileContentTextFont = Font.body.weight(.regular)
 }
