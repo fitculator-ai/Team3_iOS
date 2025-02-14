3da7ea844c65945002adbb232ea073aed1837de2
song <nadana0929@gmail.com>
2025-02-14 11:23:03 +0900

    Feat: Exercise 모델생성

---
+++ Core/Models/Exercise.swift
@@ -0,0 +1,23 @@
+//
+//  Exercise.swift
+//  Fitculator
+//
+//  Created by Song Kim on 2/14/25.
+//
+
+import Foundation
+
+public struct Exercise: Identifiable {  // <- public 추가
+    public let id = UUID()
+    public var exerciseName: String
+    public var exerciseType: String
+    public var exerciseColor: String
+    public var exerciseImage: String
+
+    public init(exerciseName: String, exerciseType: String, exerciseColor: String, exerciseImage: String) {
+        self.exerciseName = exerciseName
+        self.exerciseType = exerciseType
+        self.exerciseColor = exerciseColor
+        self.exerciseImage = exerciseImage
+    }
+}
