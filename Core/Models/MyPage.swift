74843789ee606947dd9baa4915a8d629ff46e033
Jeongheeji <w030w58@ gmail.com>
2025-02-19 17:52:40 +0900

    Feat: 사용자 정보(프로필)구조체 추가

--- Core/Models/MyPage.swift
+++ Core/Models/MyPage.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+//MARK: 유저 정보
+public struct MyPage: Identifiable {
+    
+    public let id = UUID()
+    public let userid: Int64
+    public var userName: String
+    public var userGender: String
+    public var userweight: Double
+    public var userheight: Int32
+    public var userBirth: String
+    public var socialProvider: String
+    
+    
+    public init(userid: Int64, userName: String, userGender: String, userweight: Double, userheight: Int32, userBirth: String, socialProvider: String) {
+        self.userid = userid
+        self.userName = userName
+        self.userGender = userGender
+        self.userweight = userweight
+        self.userheight = userheight
+        self.userBirth = userBirth
+        self.socialProvider = socialProvider
+    }
+}
+
+//MARK: 유저 안정시 심박수
+public struct heartUpdate {
+    public var userHeartRate: Int32
+    public let userid: Int64
+}
