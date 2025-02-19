0a636975cbafd48e6de4fcd537e9a4483dc9352a
Jeongheeji <w030w58@ gmail.com>
2025-02-19 17:58:19 +0900

    Fix: 프로필 설정 화면(ProfileEditView)/ ProfileViewModel 분리 및 몸무게 키패드 입력-> 다이얼 시트뷰 수정

---
+++ Features/Profile/Sources/View/ProfileEditView.swift
@@ -0,0 +1,348 @@
+//
+//  ProfileEditView.swift
+//  Features
+//
+//  Created by Heeji Jung on 2/14/25.
+//
+
+import SwiftUI
+import Shared
+
+struct ProfileEditView: View {
+    
+    @Environment(\.presentationMode) var presentationMode
+    @EnvironmentObject var viewModel: ProfileViewModel
+    
+    @State private var showGenderSheet = false
+    @State private var showDatePickerSheet = false
+    @State private var showWeightSheet = false
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Spacer()
+                Image(systemName: "person.circle.fill")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 100)
+                    .clipShape(Circle())
+                    .overlay(
+                        Circle()
+                            .stroke(Color.white, lineWidth: 4)
+                    )
+                    .shadow(radius: 5)
+                    .padding(.leading, 5)
+                Spacer()
+            }
+            .padding(.bottom, 10)
+
+            Spacer()
+                .frame(height: 50)
+            
+            // 닉네임
+            HStack {
+                Text("닉네임")
+                    .foregroundColor(.white)
+                    .frame(width: 100, alignment: .leading)
+                TextField("공백 없이 최대 15자까지 입력 가능", text: $viewModel.profiledata.userName)
+                    .multilineTextAlignment(.trailing)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .padding(.trailing, -20)
+            }
+            .padding(.horizontal)
+            
+            Divider()
+                .background(Color.white)
+                .frame(height: 2)
+                .padding(.top, -20)
+                .padding(.horizontal, 20)
+            
+            if !viewModel.isUserNameValid {
+                Text("닉네임을 입력해주세요.")
+                    .foregroundColor(.red)
+                    .font(.footnote)
+                    .padding(.top, 4)
+                    .padding(.horizontal)
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+            }
+
+            // 성별
+            HStack {
+                Text("성별")
+                    .foregroundColor(.white)
+                    .frame(width: 100, alignment: .leading)
+                Spacer()
+                Button(action: {
+                    showGenderSheet = true
+                }) {
+                    Text(viewModel.profiledata.userGender)
+                        .padding()
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                        .multilineTextAlignment(.trailing)
+                }
+                .padding(.trailing, -10)
+            }
+            .padding(.horizontal)
+            .padding(.top, -10)
+            
+            Divider()
+                .background(Color.white)
+                .frame(height: 2)
+                .padding(.top, -20)
+                .padding(.horizontal, 20)
+            
+            HStack {
+                Text("몸무게 (kg)")
+                    .foregroundColor(.white)
+                    .frame(width: 100)
+                    .padding(.leading, 5)
+                Spacer()
+                Button(action: {
+                    showWeightSheet = true
+                }) {
+                    Text(String(viewModel.profiledata.userweight))
+                        .padding()
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                        .multilineTextAlignment(.trailing)
+                }
+                .padding(.trailing, 5)
+            }
+            
+            Divider()
+                .background(Color.white)
+                .frame(height: 2)
+                .padding(.top, -20)
+                .padding(.horizontal, 20)
+            
+            if !viewModel.isUserWeightValid {
+                Text("올바른 몸무게를 입력해주세요.")
+                    .foregroundColor(.red)
+                    .font(.footnote)
+                    .padding(.top, 4)
+                    .padding(.horizontal)
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+            }
+
+            // 신장
+            HStack {
+                Text("신장 (cm)")
+                    .foregroundColor(.white)
+                    .frame(width: 100, alignment: .leading)
+                TextField("", text: Binding(
+                    get: { String(viewModel.profiledata.userheight) },
+                    set: { viewModel.profiledata.userheight = Int32($0) ?? 0 }
+                ))
+                .multilineTextAlignment(.trailing)
+                .keyboardType(.decimalPad)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+                .frame(maxWidth: .infinity)
+                .padding(.trailing, -15)
+            }
+            .padding(.horizontal)
+            .padding(.top, -20)
+            
+            Divider()
+                .background(Color.white)
+                .frame(height: 2)
+                .padding(.top, -20)
+                .padding(.horizontal, 20)
+            
+            if !viewModel.isUserHeightValid {
+                Text("올바른 신장을 입력해주세요.")
+                    .foregroundColor(.red)
+                    .font(.footnote)
+                    .padding(.top, 4)
+                    .padding(.horizontal)
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+            }
+
+            // 생년월일
+            HStack {
+                Text("생년월일")
+                    .foregroundColor(.white)
+                    .frame(width: 100, alignment: .leading)
+                Spacer()
+                Button(action: {
+                    showDatePickerSheet = true
+                }) {
+                    Text(viewModel.profiledata.userBirth)
+                        .padding()
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                        .multilineTextAlignment(.trailing)
+                        .padding(.trailing, -10)
+                }
+            }
+            .padding(.top, -10)
+            .padding(.horizontal)
+            
+            Divider()
+                .background(Color.white)
+                .frame(height: 2)
+                .padding(.top, -20)
+                .padding(.horizontal, 20)
+        }
+        .navigationTitle("프로필 설정")
+        .navigationBarTitleDisplayMode(.inline)
+        .navigationBarBackButtonHidden(true)
+        .navigationBarItems(
+            leading: Button(action: {
+                self.presentationMode.wrappedValue.dismiss()
+            }) {
+                Image(systemName: "chevron.left")
+                    .foregroundColor(.white)
+            },
+            trailing: Button(action: {}) {
+                Text("Save")
+                    .foregroundColor(viewModel.isFormValid ? Color.blue : Color.gray)
+                    .disabled(!viewModel.isFormValid)
+            }
+        )
+        .sheet(isPresented: $showGenderSheet) {
+            GenderSelectionSheetView(selectedGender: $viewModel.profiledata.userGender)
+                .presentationDetents([.fraction(0.35)])
+        }
+        .sheet(isPresented: $showDatePickerSheet) {
+            BirthdateDatePickerSheetView(birthdate: $viewModel.profiledata.userBirth)
+                .presentationDetents([.fraction(0.35)])
+        }
+        .sheet(isPresented: $showWeightSheet) {
+            WeightInputSheetView(Weight: $viewModel.profiledata.userweight)
+                .presentationDetents([.fraction(0.35)])
+        }
+        
+        .frame(width: UIScreen.main.bounds.width * 0.88)
+    }
+}
+
+//MARK: 프로필 선택 시트 뷰(성별)
+struct GenderSelectionSheetView: View {
+    
+    @Environment(\.presentationMode) var presentationMode
+    @Binding var selectedGender: String
+    
+    let genders = ["남성", "여성"]
+    
+    var body: some View {
+        VStack {
+            Text("성별을 선택하세요")
+                .font(.headline)
+                .padding()
+            
+            Picker("성별 선택", selection: $selectedGender) {
+                ForEach(genders, id: \.self) { gender in
+                    Text(gender)
+                }
+            }
+            .pickerStyle(WheelPickerStyle())
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+
+//MARK: 프로필 선택 시트 뷰(몸무게)
+struct WeightInputSheetView: View {
+    
+    @Environment(\.presentationMode) var presentationMode
+    @Binding var Weight: Double
+    
+    private let maxFrontValue: Double = 200
+    private let maxBackValue: Double = 9
+    
+    @State private var frontValue: Double = 70
+    @State private var backValue: Double = 0
+    
+    var body: some View {
+        VStack {
+            Text("몸무게를 입력하세요.")
+                .font(.headline)
+                .padding()
+            
+
+            HStack {
+                Picker("Front Value", selection: $frontValue) {
+                    ForEach(0...Int(maxFrontValue), id: \.self) { value in
+                        Text("\(value)").tag(Double(value))
+                    }
+                }
+                .pickerStyle(WheelPickerStyle())
+                .frame(width: 100)
+                .onChange(of: frontValue) { newValue in
+                    Weight = newValue + backValue
+                }
+                
+                Text(".")
+                    .font(.headline)
+
+                Picker("Back Value", selection: $backValue) {
+                    ForEach(0...Int(maxBackValue), id: \.self) { value in
+                        Text("\(value)").tag(Double(value))
+                    }
+                }
+                .pickerStyle(WheelPickerStyle())
+                .frame(width: 100)
+                .onChange(of: backValue) { newValue in
+                    Weight = frontValue + newValue
+                }
+                
+                Text("kg")
+                    .font(.headline)
+                    .padding()
+            }
+            .font(AppFont.subTitle)
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+
+//MARK: 프로필 선택 시트 뷰(생년월일)
+struct BirthdateDatePickerSheetView: View {
+    
+    @Environment(\.presentationMode) var presentationMode
+    @Binding var birthdate: String
+    @State private var selectedDate: Date = Date()
+    
+    var body: some View {
+        VStack {
+            Text("생년월일을 선택하세요")
+                .font(.headline)
+                .padding()
+            
+            DatePicker("", selection: $selectedDate, displayedComponents: .date)
+                .datePickerStyle(WheelDatePickerStyle())
+                .labelsHidden()
+                .font(AppFont.subTitle)
+                .onChange(of: selectedDate) { newDate in
+                    let formatter = DateFormatter()
+                    formatter.dateFormat = "yyyy-MM-dd" 
+                    birthdate = formatter.string(from: newDate)
+                }
+            
+            Spacer()
+        }
+        .padding()
+        .onAppear {
+            let formatter = DateFormatter()
+            formatter.dateFormat = "yyyy-MM-dd"
+            if let date = formatter.date(from: birthdate) {
+                selectedDate = date
+            }
+        }
+    }
+}
+
+struct ProfileEditView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileEditView()
+            .environmentObject(ProfileViewModel())
+    }
+}
--- Features/Profile/Sources/ViewModel/ProfileViewModel.swift
+++ Features/Profile/Sources/ViewModel/ProfileViewModel.swift
@@ -6,3 +6,54 @@
 //
 
 import Foundation
+import Core
+
+class ProfileViewModel: ObservableObject{
+    
+    @Published var profiledata: MyPage
+    
+    let genders = ["남성", "여성"]
+    
+    private lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none
+        return formatter
+    }()
+    
+    init() {
+        let defaultUser = MyPage(
+            userid: 12345,
+            userName: "홍길동",
+            userGender: "남성",
+            userweight: 70.0,
+            userheight: 175,
+            userBirth: "1990-12-09",
+            socialProvider: "Kakao"
+        )
+        self.profiledata = defaultUser
+    }
+    
+    //MARK: 사용자 정보 유효성 검사
+    // 유저 이름
+    var isUserNameValid: Bool {
+        return !profiledata.userName.isEmpty && profiledata.userName.count <= 15
+    }
+    
+    // 유저 몸무게
+    var isUserWeightValid: Bool {
+        return profiledata.userweight > 0
+    }
+    
+    //유저 키
+    var isUserHeightValid: Bool {
+        return profiledata.userheight > 0
+    }
+    
+    // 사용자 정보 유효성 전체 검사
+    var isFormValid: Bool {
+        return isUserNameValid && isUserWeightValid && isUserHeightValid
+    }
+    
+}
+
