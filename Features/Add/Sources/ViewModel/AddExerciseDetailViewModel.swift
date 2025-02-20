f91f7b5e5465c3a20612a97d9f0de5cc1dac63ee
song <nadana0929@gmail.com>
2025-02-20 12:23:53 +0900

    Feat: 뷰모델에서 API 통신하기

--- Features/Add/Sources/ViewModel/AddExerciseDetailViewModel.swift
+++ Features/Add/Sources/ViewModel/AddExerciseDetailViewModel.swift
@@ -6,6 +6,8 @@
 //
 
 import SwiftUI
+import Core
+import Combine
 
 class AddExerciseDetailViewModel: ObservableObject {
     @Published var averageHeartRate: String = ""
@@ -13,4 +15,39 @@ class AddExerciseDetailViewModel: ObservableObject {
     @Published var workoutDuration: String = ""
     @Published var workoutMemo: String = ""
     @Published var selectedDate = Date()
+    
+    private var cancellables = Set<AnyCancellable>()
+    private let networkService: NetworkServiceProtocol
+    
+    @Published var addWorkout: WorkoutRequest = WorkoutRequest(userId: 0,
+                                                               exerciseType: "",
+                                                               exerciseKorName: "",
+                                                               exerciseEngName: "",
+                                                               recordDate: "",
+                                                               recordStart: "",
+                                                               duration: 0,
+                                                               avgHeartRate: 0,
+                                                               highHeartRate: 0,
+                                                               memo: "")
+    
+    public init(networkService: NetworkServiceProtocol = NetworkService()) {
+        self.networkService = networkService
+    }
+    
+    public func fetchCreateWorkout(request: WorkoutRequest) {
+        networkService.request(APIEndpoint.createWorkout(request: request))
+            .receive(on: DispatchQueue.main)
+            .sink(
+                receiveCompletion: { completion in
+                    if case .failure(let error) = completion {
+                        print("Error: \(error.localizedDescription)")
+                    }
+                },
+                receiveValue: { [weak self] (response: WorkoutRequest) in
+                    print("\(response)")
+                    self?.addWorkout = response
+                }
+            )
+            .store(in: &cancellables)
+    }
 }
