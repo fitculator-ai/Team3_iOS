ce948727b7bd1519e7914e53a84c7b64b58a15fd
Jeongheeji <w030w58@ gmail.com>
2025-02-26 11:58:59 +0900

    Fix:주간운동량/피로도그래프 텍스트 .font(AppFont.subTitle)

--- Features/Profile/Sources/View/ProfileView.swift
+++ Features/Profile/Sources/View/ProfileView.swift
@@ -10,46 +10,6 @@ import Charts
 import Shared
 import Core
 
-//임시
-//enum ExerciseIntensity: String, CaseIterable {
-//    case veryLow = "매우 낮음"
-//    case low = "낮음"
-//    case medium = "중간"
-//    case high = "높음"
-//    case veryHigh = "매우 높음"
-//
-//    var color: Color {
-//        switch self {
-//        case .veryLow: return .blue
-//        case .low: return .green
-//        case .medium: return .yellow
-//        case .high: return .orange
-//        case .veryHigh: return .red
-//        }
-//    }
-//}
-
-//임시
-//struct Exercise: Identifiable {
-//    let id: Int
-//    let weeklyPercentage: [String: [ExerciseIntensityColor: Int]]
-//}
-//
-////임시
-//struct ExerciseData {
-//    static let exercises: [Exercise] = [
-//        Exercise(id: 1, weeklyPercentage: [
-//            "월요일": [.veryLow: 10, .low: 10, .medium: 30, .high: 35, .veryHigh: 5],
-//            "화요일": [.veryLow: 15, .low: 25, .medium: 25, .high: 20, .veryHigh: 5],
-//            "수요일": [.veryLow: 20, .low: 30, .medium: 20, .high: 15, .veryHigh: 5],
-//            "목요일": [.veryLow: 20, .low: 20, .medium: 20, .high: 10, .veryHigh: 0],
-//            "금요일": [.veryLow: 25, .low: 25, .medium: 10, .high: 10, .veryHigh: 5],
-//            "토요일": [.veryLow: 30, .low: 20, .medium: 10, .high: 5, .veryHigh: 0],
-//            "일요일": [.veryLow: 25, .low: 20, .medium: 15, .high: 10, .veryHigh: 5]
-//        ])
-//    ]
-//}
-
 public struct ProfileView: View {
     public init() {}
     @StateObject private var profileviewModel = ProfileViewModel()
@@ -137,7 +97,7 @@ public struct ProfileView: View {
                         
                         VStack(alignment: .leading) {
                             Text("주간 운동량")
-                                .font(AppFont.mainTitle)
+                                .font(AppFont.subTitle)
                             
                             WeeklyActivityChart()
                                 .environmentObject(chartviewModel)
@@ -146,7 +106,7 @@ public struct ProfileView: View {
                                 .frame(height: 50)
                             
                             Text("피로도 그래프")
-                                .font(AppFont.mainTitle)
+                                .font(AppFont.subTitle)
                             FatigueChart()
                                 .environmentObject(chartviewModel)
                         }
@@ -298,41 +258,6 @@ public struct FatigueChart: View {
                     VStack {
                         HStack {
                             Spacer()
-                            
-//                            Button(action: {
-//                                if let previousWeekDate = Calendar.current.date(byAdding: .weekOfYear, value: -1, to: viewModel.selectedDate) {
-//                                    viewModel.selectedDate = previousWeekDate
-//                                }
-//                            }) {
-//                                Image(systemName: "chevron.left")
-//                                    .resizable()
-//                                    .frame(width: 8, height: 18)
-//                            }
-//                            
-//                            Text(viewModel.getSelectedWeekString())
-//                                .font(.subheadline)
-//                                .fontWeight(["이번주 운동량", "지난주 운동량"].contains(viewModel.getSelectedWeekString()) ? .semibold : .regular)
-//                                .padding([.leading, .trailing], 24)
-//                                .padding([.top, .bottom], 8)
-//                                .frame(width: 228)
-//                                .background(Color.cellColor)
-//                                .clipShape(RoundedRectangle(cornerRadius: 36))
-//                                .onTapGesture {
-//                                    showDatePicker.toggle()
-//                                }
-//                            
-//                            Button(action: {
-//                                if let nextWeekDate = Calendar.current.date(byAdding: .weekOfYear, value: +1, to: viewModel.selectedDate) {
-//                                    viewModel.selectedDate = nextWeekDate
-//                                }
-//                            }) {
-//                                Image(systemName: "chevron.right")
-//                                    .resizable()
-//                                    .frame(width: 8, height: 18)
-//                            }
-//                            .disabled(viewModel.getSelectedWeekString() == "이번주 운동량")
-//                            
-//                            Spacer()
                         }
                         
                         VStack {
@@ -350,53 +275,34 @@ public struct FatigueChart: View {
                                     .background(RoundedRectangle(cornerRadius: 10).fill(Color.black.opacity(0.5)))
                                     .padding()
                             } else {
-                                // Combine workout data
                                 let workoutDataRecords = viewModel.workoutData?.records ?? []
                                 let weeklyWorkoutDataRecords = viewModel.weeklyWorkoutData.flatMap { $0.records }
                                 let combinedData = workoutDataRecords + weeklyWorkoutDataRecords
-                                
-                                // Create line data outside the chart
-                                let lineData = combinedData.map { record in
-                                    (record.recordStart, ExerciseIntensityColor.from(record.intensity).intensityValue)
-                                }
+
+                                let lineData: [(Date, Double)] = combinedData
+                                    .map { record -> (Date, Double)? in
+                                        guard let date = DateFormatterUtil.dateFormatTime.date(from: record.recordStart) else {
+                                            print("날짜 변환 실패: \(record.recordStart)")
+                                            return nil
+                                        }
+                                        return (date, ExerciseIntensityColor.from(record.intensity).intensityValue) as? (Date, Double)
+                                    }
+                                    .compactMap { $0 }
                                 
                                 Chart {
-                              
-//                                    ForEach(combinedData, id: \.id) { record in
-//                                        let recordDate = record.recordStart
-//                                        let intensityLevel = ExerciseIntensityColor.from(record.intensity)
-//                                        let intensityValue = intensityLevel.intensityValue
-//
-//                                        BarMark(
-//                                            x: .value("Date", recordDate),
-//                                            y: .value("Intensity", intensityValue)
-//                                        )
-//                                        .foregroundStyle(intensityLevel.color)
-//                                    }
-
-//                                    let lineData: [(Date, Double)] = combinedData.map { record in
-//                                        return (record.recordStart, Double(ExerciseIntensityColor.from(record.intensity).intensityValue))
-//                                    }
-
-                                    ForEach(lineData.indices, id: \.self) { index in
-                                        if index > 0 {  // 첫 번째 데이터를 제외하고 연결
-                                            let prevData = lineData[index - 1]
-                                            let currentData = lineData[index]
-
-                                            LineMark(
-                                                x: .value("Date", currentData.0),
-                                                y: .value("Intensity", currentData.1)
-                                            )
-                                            .foregroundStyle(Color.blue)
-                                            .lineStyle(.init(lineWidth: 2))
-                                        }
+                                    ForEach(lineData, id: \.0) { data in
+                                        LineMark(
+                                            x: .value("Date", data.0, unit: .day), // ✅ Date 값 올바르게 사용
+                                            y: .value("Intensity", data.1)
+                                        )
+                                        .foregroundStyle(Color.blue)
+                                        .lineStyle(.init(lineWidth: 2))
                                     }
                                 }
                                 .frame(height: 300)
                                 .padding(.horizontal, 8)
                             }
                         }
-                        
                     }
                 }
             }
